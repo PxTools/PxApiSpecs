ffe4886976cbd55c384d96e4dcb98987d8e9da0d
JohannesFinsveen <56305961+JohannesFinsveen@users.noreply.github.com>
2022-04-05 09:32:35 +0200

    Rename seems to work, adding some search classes

---
+++ PxApi2Dummy/PxApi2Dummy/Models/SearchResultItem.cs
@@ -0,0 +1,41 @@
+﻿namespace PxApi2Dummy.Models
+{
+    public class SearchResultItem:MenuItem
+    {
+
+        /// <summary>
+        /// Id of source application instance where the table is found.
+        /// </summary>
+        public string sourceId { get; set; }
+
+        /// <summary>
+        /// Search score (relevance of search)
+        /// </summary>
+        public float score { get; set; }
+
+
+
+        public SearchResultItem() 
+        {
+        }
+
+        public SearchResultItem(Source source, ApiV1.SearchResultItem oldItem, string lang)
+        {
+            this.sourceId = source.Id;
+
+            menuItemType = MenuItemType.TABLE;
+
+            id = oldItem.id;
+            label = oldItem.title;
+            updated = oldItem.published;
+            score = oldItem.score;
+
+            string tableUrl = String.Format(source.SearchToTableFormat[lang], oldItem.mypath, oldItem.id);
+            
+            links = new Link[1];
+            Link tmp = new Link { href = tableUrl, rel = "metadata" };
+            links[0] = tmp;
+
+        }
+    }
+}
---
+++ PxApi2Dummy/PxApi2Dummy/Search/DoSearch.cs
@@ -0,0 +1,49 @@
+﻿using PxApi2Dummy.Models;
+
+namespace PxApi2Dummy.Search
+{
+    public class DoSearch
+    {
+
+        public static SearchResult Get(string searchText)
+        {
+            SearchResult myOut = new SearchResult();
+            myOut.paginationStuff = "do to";
+            
+            string lang = "no";
+            HttpClient _httpClient = new HttpClient();
+
+            List<SearchResultItem> tmp = new List<SearchResultItem>();
+
+            ConfigInfo conf = SampleData.SampleConfigInfo.Get();
+            foreach (Source s in conf.sources)
+            {
+                //if s.apiVersjon
+                string url = s.SearchUrl[lang]+searchText;
+                string jsonString = _httpClient.GetStringAsync(url).Result;
+                List<SearchResultItem> fromOneSource =  readJson(jsonString, s, lang);
+               tmp.AddRange(fromOneSource);
+
+            }
+            myOut.data = tmp.ToArray();
+            return myOut;
+        }
+
+
+        private static List<SearchResultItem> readJson(string jsonString, Source source, string lang)
+        {
+            List<SearchResultItem> myOut = new List<SearchResultItem>();
+
+            var old = Utf8Json.JsonSerializer.Deserialize<Models.ApiV1.SearchResultItem[]>(jsonString);
+            foreach (var item in old)
+            {   
+                myOut.Add(new SearchResultItem(source, item, lang));
+            }
+            return myOut;
+        }
+
+        
+
+
+    }
+}
