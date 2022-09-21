55b043741ccea06f7535300a376bc06e57edaa69
JohannesFinsveen <56305961+JohannesFinsveen@users.noreply.github.com>
2022-09-21 17:01:54 +0200

    Had to remove nullable from priceType and "force" swashbuckle, but it now seems the generated solution builds as generated

--- generator/myGen.bat
+++ generator/myGen.bat
@@ -3,15 +3,20 @@ rem swagger uses -l aspnetcore oaTools uses -g aspnetcore
 
 set gen_swag=swaggerapi/swagger-codegen-cli-v3:3.0.35
 set gen_oaTool=openapitools/openapi-generator-cli
-set props1=--additional-properties=aspnetCoreVersion=6.0 --additional-properties=nullableReferenceTypes=true
-set props2=--additional-properties=buildTarget=library --additional-properties=packageName=PCAxis.OpenAPILib
+set props1=--additional-properties=aspnetCoreVersion=6.0 --additional-properties=nullableReferenceTypes=true 
+set props2=--additional-properties=buildTarget=library --additional-properties=packageName=PCAxis.OpenAPILib 
+set props3=--additional-properties=useSeperateModelProject=true
+set props4=--additional-properties=legacyDiscriminatorBehavior=false
+set props5=--additional-properties=swashbuckleVersion=6.4.0 --additional-properties=useSwashbuckle=true
+
+set myProps=%props1% %props2% %props3% %props4% %props5%
 
 set yaml_doc=/local/spec.yaml
 rem set yaml_doc=https://raw.githubusercontent.com/statisticssweden/PxApiSpecs/feature/tablemetadata/PxAPI-2.yml
 
 
 
-echo hei %props1%
+echo hei
 
 rem docker run --rm -v %cd%:/local %gen_oaTool% help > openapi_help.txt
 rem docker run --rm -v %cd%:/local %gen_oaTool% help generate > openapi_help_generate.txt
@@ -20,7 +25,7 @@ rem docker run --rm -v %cd%:/local %gen_oaTool% config-help -g aspnetcore > open
 @echo on
 copy ..\PxAPI-2.yml .\spec.yaml
 
-docker run --rm -v %cd%:/local %gen_oaTool% generate -i %yaml_doc% -g aspnetcore -o /local/out/from_spec %props1% %props2%  > log.txt
+docker run --rm -v %cd%:/local %gen_oaTool% generate -i %yaml_doc% -g aspnetcore -o /local/out %myProps%  > log.txt
 @echo off 
 
 rem docker run --rm -v %cd%:/local %gen_swag% generate -i /local/spec.yaml -l aspnetcore -o /local/out/fra_spec1 --additional-properties=aspnetCoreVersion=6.0 --additional-properties=alalalala=6.0 --additional-properties=nullableReferenceTypes=true  > fra_spec1.txt
